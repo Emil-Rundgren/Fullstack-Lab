6308dc811b54b0bed494faf41232aefbda480691
Emil Rundgren <144038584+Emil-Rundgren@users.noreply.github.com>
2024-06-04 22:09:28 +0200

    Jag tog bort componenten productOffers och jag gjorde navbaren responsiv samt så möjliggjorde jag navigering när man klickar på det olika länkarna.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-// Navbar.tsx
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -7,37 +6,170 @@ import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import LoginIcon from "@mui/icons-material/Login";
+import Grid from "@mui/material/Grid";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const Navbar: React.FC = () => {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position="static"
-        color="default"
-        sx={{ borderBottom: "3px solid lightblue", width: "100%" }}
-      >
-        <Toolbar
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  if (!isMobile) {
+    // Desktop Layout
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="static"
+          color="default"
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+            borderBottom: "3px solid lightblue",
+            width: "100%",
+            marginTop: 1,
           }}
         >
-          <Box sx={{ flex: 2, textAlign: "center" }}>
-            <Typography variant="h6" component="div">
-              E-LAPTOP
-            </Typography>
-          </Box>
-          <Box
+          <Toolbar>
+            <Grid container alignItems="center" spacing={2}>
+              <Grid item xs={2}>
+                <Link href="/" color="inherit" underline="none">
+                  <Typography variant="h6" component="div">
+                    E-LAPTOP
+                  </Typography>
+                </Link>
+              </Grid>
+              <Grid item xs={1} />
+              <Grid item xs={6}>
+                <Box
+                  component="form"
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    backgroundColor: "white",
+                    borderRadius: 1,
+                    border: "1px solid #ccc",
+                    flexGrow: 1,
+                    width: "100%",
+                  }}
+                >
+                  <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Search"
+                    inputProps={{ "aria-label": "search" }}
+                  />
+                  <IconButton
+                    type="submit"
+                    sx={{ p: "10px" }}
+                    aria-label="search"
+                  >
+                    <SearchIcon />
+                  </IconButton>
+                </Box>
+              </Grid>
+              <Grid item xs={1} />
+              <Grid item xs={1}>
+                <Link
+                  href="/LogIn"
+                  color="inherit"
+                  underline="none"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <IconButton color="inherit">
+                    <LoginIcon />
+                  </IconButton>
+                  <Typography variant="body2">Logga in</Typography>
+                </Link>
+              </Grid>
+              <Grid item xs={1}>
+                <Link
+                  href="/Cart"
+                  color="inherit"
+                  underline="none"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <IconButton color="inherit">
+                    <ShoppingCartIcon />
+                  </IconButton>
+                  <Typography variant="body2">Kundvagn</Typography>
+                </Link>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  } else {
+    // Mobile Layout
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="static"
+          color="default"
+          sx={{ borderBottom: "3px solid lightblue", width: "100%" }}
+        >
+          <Toolbar
             sx={{
-              flex: 5,
               display: "flex",
-              justifyContent: "center",
+              flexDirection: "column",
               alignItems: "center",
             }}
           >
+            <Box
+              sx={{
+                display: "flex",
+                width: "100%",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Link href="/" color="inherit" underline="none">
+                <Typography variant="h6" component="div">
+                  E-LAPTOP
+                </Typography>
+              </Link>
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <Link
+                  href="/LogIn"
+                  color="inherit"
+                  underline="none"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    marginRight: 2,
+                  }}
+                >
+                  <IconButton color="inherit">
+                    <LoginIcon />
+                  </IconButton>
+                  <Typography variant="body2">Logga in</Typography>
+                </Link>
+                <Link
+                  href="/Cart"
+                  color="inherit"
+                  underline="none"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <IconButton color="inherit">
+                    <ShoppingCartIcon />
+                  </IconButton>
+                  <Typography variant="body2">Kundvagn</Typography>
+                </Link>
+              </Box>
+            </Box>
             <Box
               component="form"
               sx={{
@@ -46,8 +178,8 @@ const Navbar: React.FC = () => {
                 backgroundColor: "white",
                 borderRadius: 1,
                 border: "1px solid #ccc",
-                flexGrow: 1,
-                maxWidth: "100%",
+                width: "100%",
+                marginTop: 1,
               }}
             >
               <InputBase
@@ -59,20 +191,11 @@ const Navbar: React.FC = () => {
                 <SearchIcon />
               </IconButton>
             </Box>
-          </Box>
-          <Box
-            sx={{ flex: 3, display: "flex", justifyContent: "space-around" }}
-          >
-            <Button color="inherit" href="/LogIn">
-              <Link>Logga in</Link>
-            </Button>
-            <Button color="inherit">Kundvagn</Button>
-            <Button color="inherit">Meny</Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  }
 };
 
 export default Navbar;
